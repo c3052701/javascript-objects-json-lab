5e74959c219488150d66c363aae2a0a187de7ed3
Rimu <c3052701@hallam.shu.ac.uk>
2023-11-24 10:02:10 +0000

    displaying the time

--- Js/clock.js
+++ Js/clock.js
@@ -9,5 +9,11 @@ myNode.innerHTML = "Welcome";
 const myDate = new Date();
 console.dir(myDate);
 
+// show only hours, minutes and seconds
+myNode.innerHTML = myDate.toLocaleTimeString([], 
+{ hour: "2-digit", minute: "2-digit", second: "2-digit" });
+
+
+
 // codes ends here
 })();

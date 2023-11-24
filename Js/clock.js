226ac8afd3bc4b260f16e1c9d99328891fca4cc7
Rimu <c3052701@hallam.shu.ac.uk>
2023-11-24 09:48:22 +0000

    Welcome message added

---
+++ Js/clock.js
@@ -0,0 +1,10 @@
+(function(){ 
+//codes starts here
+console.info("Hello JS"); 
+const myNode = document.createElement("div");
+const digitalclock = document.getElementById("digitalClock");
+digitalClock.appendChild(myNode);
+myNode.innerHTML = "Welcome";
+
+// codes ends here
+})();
